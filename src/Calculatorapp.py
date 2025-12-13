e0a1a9819f5d4baa7be8e000a472d43a6523ccd7
Kendric Jones <Kendric888@outlook.comexit>
2025-12-12 20:02:21 -0600

    Added new operations and pytest associated with each op

--- src/Calculatorapp.py
+++ src/Calculatorapp.py
@@ -1,3 +1,42 @@
+import math
+
 def add(a, b):
     return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Divised by Zero")
+    else:
+        return a / b
     
+def log10(x):
+    if x <= 0:
+        raise ValueError("Undefined for non-positive values")
+    else:
+        return math.log10(x)
+    
+def square(x):
+    return x ** 2
+
+def sqrt(x):
+    if x < 0:
+        raise ValueError("Number must be non-negative")
+    else:
+        return math.sqrt(x)
+
+def sin(x):
+    return math.sin(math.radians(x))
+
+
+def cos(x):
+    return math.cos(math.radians(x))
+
+
+def percentage(x):
+    return x / 100
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,74 @@
 import sys
 from pathlib import Path
+import pytest
 
 root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
 
-from Calculatorapp import add
+from Calculatorapp import add, subtract, multiplication, division, log10, square, sqrt, sin, cos, percentage
 
 def test_add():
     assert add(5, 6) == 11
 
 def test_add2():
     assert add(5, 6) != 10
+
+def test_subtract():
+    assert subtract(10, 6) == 4
+
+def test_subtract2():
+    assert subtract(10, 6) != 5
+
+def test_multiplication():
+    assert multiplication(2, 8) == 16
+
+def test_multiplication2():
+    assert multiplication(2, 8) != 19
+
+def test_division():
+    assert division(6, 3) == 2
+
+def test_division2():
+    assert division(6, 3) != 3
+
+def test_division_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        division(6, 0)
+
+def test_log10():
+    with pytest.raises(ValueError):
+        log10(-1)
+
+def test_log10_2():
+    assert log10(10) == 1
+
+def test_log10_3():
+    assert log10(10) != 5
+
+def test_square():
+    assert square(5) == 25
+
+def test_square2():
+    assert square(5) != 30
+
+def test_sqrt():
+    assert sqrt(36) == 6
+
+def test_sqrt2():
+    assert sqrt(36) != 10
+
+def test_sqrt_negative():
+    with pytest.raises(ValueError):
+        sqrt(-36)
+
+def test_sin():
+    assert abs(sin(90) - 1.0) < 1e-6
+
+def test_cos():
+    assert abs(cos(0) - 1.0) < 1e-6
+
+def test_percentage():
+    assert percentage(50) == 0.5
+
+def test_percentage2():
+    assert percentage(50) != 0.4
